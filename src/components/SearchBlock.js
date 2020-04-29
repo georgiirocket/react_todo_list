6dae277cc5b507d000ecc5867f048bb4c9453bc5
Georgii Romanishko <georgiiromanishko@tut.by>
2020-04-29 21:55:19 +0300

    restructering app

---
+++ src/components/SearchBlock.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SearchBlock = () => {
+
+    const SearchBlockText = 'What are you wont to search';
+    return <input placeholder={SearchBlockText} />;
+}
+
+export default SearchBlock;  
--- src/index.js
+++ src/index.js
@@ -1,29 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const AppHeader = () => {
-  return <h1>Todo List</h1>;
-}
+import AppHeader from './components/appHeader';
+import SearchBlock from './components/SearchBlock';
+import TodoList from './components/TodoList';
 
-const SearchBlock = () => {
-  return <input placeholder="search" />;
-}
-
-const TodoList = () => {
+const App = () => {
   return (
-    <ul>
-      <li>learn JS</li>
-      <li>learn React</li>
-    </ul>
+    <div>
+      <AppHeader />
+      <SearchBlock />
+      <TodoList />
+    </div>
   );
 }
 
-const el = (
-  <div>
-    <AppHeader />
-    <SearchBlock />
-    <TodoList />
-  </div>
-);
 
-ReactDOM.render(el, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
